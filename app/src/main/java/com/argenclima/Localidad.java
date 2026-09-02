231c0e7ab015e7b4bbf8ad6f69ed191c84d36f14
Jeremías Agustín Martinez Ciucio <jeremias@jeremiasmartinez.com>
2026-09-02 11:53:09 -0300

    style: acomodar indentado

--- app/src/main/java/com/argenclima/Localidad.java
+++ app/src/main/java/com/argenclima/Localidad.java
@@ -3,25 +3,25 @@ import java.util.HashMap;
 
 public class Localidad implements Lugar{
 
-    HashMap<Integer,Double> temperaturas = new HashMap<Integer,Double>();
+  HashMap<Integer,Double> temperaturas = new HashMap<Integer,Double>();
 
-    private String nombre;
-    private double temperaturaMedia;
+  private String nombre;
+  private double temperaturaMedia;
 
-    public Localidad(String nombre) {
-        this.nombre = nombre;
-    }
+  public Localidad(String nombre) {
+    this.nombre = nombre;
+  }
 
-    public String getNombre() {
-        return nombre;
-    }
+  public String getNombre() {
+    return nombre;
+  }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-    public double temperaturaPromedio(int anio){
-        return temperaturas.get(anio);
-    }
+  public double temperaturaPromedio(int anio){
+    return temperaturas.get(anio);
+  }
 
 }
